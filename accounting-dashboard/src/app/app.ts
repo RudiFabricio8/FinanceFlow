7f03476633ea32d1f13ca0f683c1d750a51299e0
grafica <fabriciojaimes775@gmail.com>
2025-10-19 12:57:08 -0600

    feat: Configurar app.ts y eliminar archivo de pruebas

--- accounting-dashboard/src/app/app.ts
+++ accounting-dashboard/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+// src/app/app.ts
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './shared/components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('accounting-dashboard');
+export class AppComponent {
+  title = 'accounting-dashboard';
 }
